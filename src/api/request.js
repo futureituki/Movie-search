aa349128029e3919079e6dced1d0c3f3db9a63de
CreateWebSiteTeam <yourgitemail@example.com>
2022-06-09 03:07:45 +0900

    tailwind api導入

---
+++ src/api/request.js
@@ -0,0 +1,5 @@
+const API_KEY = "AIzaSyDl7qDcRIsgaa-Fvn2dLlE57EQ4Hv1UbBs";
+
+export const requests ={
+  fetchFreeDrinkFood:`videos?id=7lCDEYXw3mM&key=${API_KEY}&part=snippet,contentDetails,statistics,status`,
+}
